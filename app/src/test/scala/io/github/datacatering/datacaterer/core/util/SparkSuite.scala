fe071928293added95388cc1f22625b6d471411b
Flook Peter <pflooky@hotmail.com>
2024-06-05 14:46:53 +0800

    Remove commented spark configs from test

--- app/src/test/scala/io/github/datacatering/datacaterer/core/util/SparkSuite.scala
+++ app/src/test/scala/io/github/datacatering/datacaterer/core/util/SparkSuite.scala
@@ -15,12 +15,6 @@ trait SparkSuite extends AnyFunSuite with BeforeAndAfterAll with BeforeAndAfterE
       .config("spark.ui.enabled", "false")
       //      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") //used for hudi
       .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,io.delta.sql.DeltaSparkSessionExtension")
-//      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-//      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-//      .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog")
-//      .config("spark.sql.catalog.local.type", "hadoop")
-//      .config("spark.sql.catalog.local.warehouse", "/tmp/iceberg/warehouse")
-//      .config("spark.sql.defaultCatalog", "local")
       .getOrCreate()
   }
 
